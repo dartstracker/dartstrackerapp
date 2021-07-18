4aec5c453c8a29628d0d6eb1c1751580e09f67fa
Joe <joefrizzell@gmail.com>
2021-07-17 21:10:47 -0400

    displaying database names

--- dartstracker.js
+++ dartstracker.js
@@ -2,10 +2,7 @@ const {MongoClient} = require('mongodb');
 const http = require('http');
 
 async function main() {
-  /**
-   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-   */
+
   const uri = "mongodb+srv://" + process.env.DB_USERNAME + ":" + process.env.DB_PASSWORD + "@dartstracker.j7gzq.mongodb.net/dartstracker?retryWrites=true&w=majority";
   const client = new MongoClient(uri);
   try {
@@ -23,8 +20,9 @@ async function listDatabases(client){
     databasesList = await client.db().admin().listDatabases();
     var server = http.createServer(function(req, res) {
         res.writeHead(200, {'Content-Type': 'text/plain'});
-        var response = databasesList.databases.db.name.join('\n');
-        res.end(response);
+        var databaseNames = [];
+        databasesList.databases.forEach(db => databaseNames.push(` - ${db.name}`));
+        res.end(databaseNames.join("\n"));
     });
     server.listen();
 }
