1c26f00475f590ab81e1ad0be16502456a3e802c
Joe <joefrizzell@gmail.com>
2021-08-01 14:31:51 -0400

    adding modules and package.json

--- dartstracker.js
+++ dartstracker.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 async function main() {
 
-  const uri = "mongodb+srv://" + process.env.DB_USERNAME + ":" + process.env.DB_PASSWORD + "@dartstracker.j7gzq.mongodb.net/dartstracker?retryWrites=true&w=majority";
+  const uri = 'mongodb+srv://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@dartstracker.j7gzq.mongodb.net/dartstracker?retryWrites=true&w=majority';
   const client = new MongoClient(uri);
   try {
       await client.connect();
@@ -19,10 +19,14 @@ main().catch(console.error);
 async function listDatabases(client){
     databasesList = await client.db().admin().listDatabases();
     var server = http.createServer(function(req, res) {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        var databaseNames = [];
-        databasesList.databases.forEach(db => databaseNames.push(` - ${db.name}`));
-        res.end(databaseNames.join("\n"));
+      res.writeHead(200, {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+      });
+        var databaseJson = JSON.stringify(databasesList.databases);
+        res.end(databaseJson);
     });
     server.listen();
 }
