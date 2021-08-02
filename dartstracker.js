18fa5a117e9583bc3ec3dd04727c9ff9ac552c4a
Joe <joefrizzell@gmail.com>
2021-08-02 00:13:16 -0400

    adding socket.io and code comments

--- dartstracker.js
+++ dartstracker.js
@@ -1,26 +1,40 @@
-const {MongoClient} = require('mongodb');
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 8000;
+//TODO: Figure out socket.io
 
+// dotenv pulls in environment variables from .env file
+require('dotenv').config();
+
+// setting up mongodb
+const {MongoClient} = require('mongodb');
+const port = process.env.PORT || 8000;
 const uri = 'mongodb+srv://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@dartstracker.j7gzq.mongodb.net/dartstracker?retryWrites=true&w=majority';
 const client = new MongoClient(uri);
 
+// setting up express
+const express = require('express');
+const app = express();
+
+// start express server
 app.listen(port, function() {
   console.log('listening on ' + port);
-})
+});
+
+// root server handling
 app.get('/', (req, res) => {
   if(originDefined(req, res)){
-    res.set({
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-      'Access-Control-Allow-Methods': 'GET'
-    });
+    setGetHeaders(res);
     main(res);
   }
+});
+
+// save game handling
+app.post('/saveGame', function (req, res) {
+  if(originDefined(req, res)){
+    setPostHeaders(res);
+    saveGame(req, res);
+  }
 })
+
+// function to check if the origin is defined in the request headers, redirects if origin isn't set
 function originDefined(req, res){
   reqOrigin = req.get('origin');
   if(typeof reqOrigin == 'undefined'){
@@ -30,6 +44,28 @@ function originDefined(req, res){
     return true;
   }
 }
+
+// setting headers for a GET request
+function setGetHeaders(res){
+  res.set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'Access-Control-Allow-Methods': 'GET'
+  });
+}
+
+// setting headers for a POST request
+function setPostHeaders(res){
+  res.set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'Access-Control-Allow-Methods': 'POST'
+  });
+}
+
+// function that is called from root
 async function main(res) {
   try {
       await client.connect();
@@ -41,10 +77,12 @@ async function main(res) {
     await client.close();
   }
 }
+
+// function to return all the saved games in db
 async function getGamesCount(client, res){
   gamesArray = [];
   cursor = client.db('dartstracker').collection('games').find();
-  await cursor.forEach( function(item){
+  await cursor.forEach(function(item){
     gamesArray.push(item);
   });
   res.send(JSON.stringify(gamesArray));
