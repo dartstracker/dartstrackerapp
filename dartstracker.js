8acca75c36dbbb529c73e344caa380b7fca2c5c4
Joe <joefrizzell@gmail.com>
2021-08-22 22:14:06 -0400

    adding socket.io

--- dartstracker.js
+++ dartstracker.js
@@ -1,10 +1,9 @@
-//TODO: Figure out socket.io
-
 // dotenv pulls in environment variables from .env file
 require('dotenv').config();
 
 // setting up mongodb
 const {MongoClient} = require('mongodb');
+const ObjectID = require('mongodb').ObjectId;
 const port = process.env.PORT || 8000;
 const uri = 'mongodb+srv://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@dartstracker.j7gzq.mongodb.net/dartstracker?retryWrites=true&w=majority';
 const client = new MongoClient(uri);
@@ -12,78 +11,112 @@ const client = new MongoClient(uri);
 // setting up express
 const express = require('express');
 const app = express();
-
-// start express server
-app.listen(port, function() {
-  console.log('listening on ' + port);
-});
-
-// root server handling
-app.get('/', (req, res) => {
-  if(originDefined(req, res)){
-    setGetHeaders(res);
-    main(res);
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin:"*",
+    methods: ["GET", "POST"]
   }
 });
 
-// save game handling
-app.post('/saveGame', function (req, res) {
-  if(originDefined(req, res)){
-    setPostHeaders(res);
-    saveGame(req, res);
-  }
-})
-
-// function to check if the origin is defined in the request headers, redirects if origin isn't set
-function originDefined(req, res){
-  reqOrigin = req.get('origin');
-  if(typeof reqOrigin == 'undefined'){
-    res.redirect('https://dartstracker.github.io');
-    return false;
-  } else {
-    return true;
-  }
-}
-
-// setting headers for a GET request
-function setGetHeaders(res){
-  res.set({
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-    'Access-Control-Allow-Methods': 'GET'
+io.on('connection', (socket) => {
+  socket.on('new game', (gameType) => {
+    newGame(socket, gameType);
   });
-}
-
-// setting headers for a POST request
-function setPostHeaders(res){
-  res.set({
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-    'Access-Control-Allow-Methods': 'POST'
+  socket.on('join game', (gameId) => {
+    joinGame(socket, gameId)
   });
-}
+  socket.on('save game', (gameObject) => {
+    saveGame(socket, gameObject)
+  });
+});
+io.of("/").adapter.on("join-room", (room, id) => {
+  if(room != id){
+    var thisSocket = io.sockets.sockets.get(id);
+    thisSocket.broadcast.to(room).emit('player joined', id);
+    console.log(`socket ${id} has joined room ${room}`);
+  }
+});
 
-// function that is called from root
-async function main(res) {
+io.of("/").adapter.on("leave-room", (room, id) => {
+  if(room != id){
+    var thisSocket = io.sockets.sockets.get(id);
+    thisSocket.broadcast.to(room).emit('player left', id);
+    console.log(`socket ${id} has left room ${room}`);
+  }
+});
+
+http.listen(port, function() {
+   console.log('listening on *:' + port);
+});
+// function to create new game
+async function newGame(socket, gameType){
   try {
-      await client.connect();
-      await getGamesCount(client, res);
+    gameShell = {
+      gameType: gameType
+    }
+    await client.connect();
+    theDB = client.db('dartstracker');
+    theDB.collection('games').insertOne(gameShell, function(err, res) {
+      if (err) {
+        io.to(socket.id).emit('error', err);
+      } else {
+        io.to(socket.id).emit('game created', res);
+      }
+      client.close();
+    });
   } catch (e) {
       console.error(e);
-      res.send(e);
-  } finally {
-    await client.close();
+      io.to(socket.id).emit('error', e);
+      client.close();
   }
 }
 
-// function to return all the saved games in db
-async function getGamesCount(client, res){
-  gamesArray = [];
-  cursor = client.db('dartstracker').collection('games').find();
-  await cursor.forEach(function(item){
-    gamesArray.push(item);
-  });
-  res.send(JSON.stringify(gamesArray));
+// function to join a game
+async function joinGame(socket, gameId){
+  try {
+    gameShell = {
+      _id: new ObjectID(gameId)
+    }
+    await client.connect();
+    theDB = client.db('dartstracker');
+    theDB.collection('games').findOne(gameShell, function(err, res) {
+      if (err) {
+        io.to(socket.id).emit('error', err);
+      } else {
+        io.to(socket.id).emit('game joined', res);
+        socket.join(gameId);
+      }
+      client.close();
+    });
+  } catch (e) {
+      console.error(e);
+      io.to(socket.id).emit('error', e);
+      client.close();
+  }
+}
+// function to save a game
+async function saveGame(socket, gameObject){
+  try {
+    await client.connect();
+    theDB = client.db('dartstracker');
+    let query = { _id: new ObjectID(gameObject._id)};
+    let replacement = {
+      gameType: gameObject.gameType,
+      players: gameObject.players,
+      states: gameObject.states
+    }
+    theDB.collection('games').replaceOne(query, replacement, function(err, res) {
+      if (err) {
+        io.to(socket.id).emit('error', err);
+      } else {
+        socket.broadcast.to(gameObject._id).emit('game updated', gameObject);
+      }
+      client.close();
+    });
+  } catch (e) {
+      console.error(e);
+      io.to(socket.id).emit('error', e);
+      client.close();
+  }
 }
